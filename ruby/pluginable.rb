c2528be0d0cad919ee7e00a04847a37249e00a99
Jean-Denis Vauguet <jd@vauguet.fr>
2010-03-22 09:52:52 +0100

    comments

--- ruby/pluginable.rb
+++ ruby/pluginable.rb
@@ -61,22 +61,71 @@ class Object
   end
 end
 
+# This module adds pluginable behavior to some module or class.
+# It means the module or class will be able to activate plugins
+# which have been written on the purpose of adding new or redefining
+# existing behaviors (methods).
+#
+# Plugins can alter class or instance methods, within modules or classes.
+# A convention in plugins definitions makes it possible to auto-discover
+# which modules or classes of the receiver are concerned by the plugin
+# to be activated. Those modules or classes will gain some new behavior
+# if the plugin declares so, while retaining the ability to fallback on
+# their original behavior via simple inheritance. Pluginable will also
+# automagically hook class and instance methods in their proper location
+# if a simple structure convention is followed in the plugin definition.
+#
+# TODO
+# The hooking process can be configured (no more automagical hooking)
+# using the unpack method each plugin may provide:
+#   def unpack
+#     # ... design pending
+#   end
+#
+# If several plugins are activated and performs some redefinitions
+# in a class or module of the receiver, one has to pay extra attention
+# to chained behavior and inconsistency. It's good practice to always
+# call super at some point so as to traverse the all plugins inheritance
+# chain until the original definition is reached.
+#
+# If you want to completely overwrite the original behavior while being
+# able to activate multiple plugins, a nice way to go is to write a
+# SuperPlugin which redefines the method you're targeting at, without
+# calling super. Activate this SuperPlugin first, then activate the other
+# plugins, using super in their redefinitions so as to reach the SuperPlugin
+# implementation in the end.
+#
+# TODO
+# If you want to bold-skip a plugin once, you may first check wether
+# it's activated using MyProject.plugins. If so, you may then call
+# <code>MyProject.bypass :plugin_name</code>, which will perform
+# <code>shutdown</code> and <code>activate</code> in sequence while
+# retaining the inheritance position of the bypassed plugin.
 module Pluginable
   extend self
 
+  # FIXME
+  # en fait, à part activate, je dois pouvoir tout passer en private, non ?
+
+  # FIXME
+  # le transformer en attr_reader peut-être ?
+  # The base module or class which called extend Pluginable
   def self.receiver
     @receiver ||= nil
   end
 
+  # This hash holds the plugins hooks. Its structure is like this:
+  # a module or class within the receiver => array of plugins performing redef on it
   def self.redefinable
     @redefinable ||= {}
   end
 
+  # A list of all active plugins for the receiver.
   def self.plugins
     @plugins ||= []
   end
 
-  def self.extended base
+  def self.extended base #:nodoc:
     puts "Initializing Pluginable..."
     @receiver = base
     puts @receiver
@@ -95,10 +144,33 @@ module Pluginable
     puts "> redefinable: #{redefinable.inspect}"
     puts "Plugins init done."
     puts
-  end 
+  end
 
+  # Activate a plugin.
+  #
+  # Must be called by the receiver, ie. the module or class which called
+  # <code>extend Pluginable</code>.
+  #
+  #   module MyProject
+  #     extend Pluginable
+  #
+  #     class Foo
+  #       # ...
+  #     end
+  #
+  #     class Server
+  #       def initialize
+  #         MyProject.activate :some_super_plugin_I_wrote
+  #         MyProject.activate "anotherPlugin"
+  #       end
+  #     end
+  #   end
+  #
   def activate plugin_name
     # be careful about self here, need to explicit Pluginable
+
+    # TODO: gérer en arguments les chaines et les symboles,
+    # camelcased on underscored (faire un helper private)
     
     plugin_name = plugin_name.capitalize
 
@@ -114,7 +186,6 @@ module Pluginable
       e = Pluginable.receiver.const_get e
 
       puts
-      #puts e.class_eval("class << self; self; end").ancestors.inspect
       e.extend ::Pluginable::PluginInit
       puts
       Pluginable.redefinable[e] << Pluginable.receiver.const_get("Plugin").const_get(plugin_name) if Pluginable.redefinable.has_key? e
@@ -123,13 +194,25 @@ module Pluginable
     puts Pluginable.redefinable.inspect
   end
 
+  # TODO
+  # def shutdown
+  # end
+
+  # TODO
+  # def bypass
+  # end
+
+  # This module is responsible for extending class instances with
+  # new behavior defined by some plugin(s). It's the responsability
+  # of the plugins to call super or not so as to fallback on the
+  # original behavior: this module only has the hooks up and running.
   module PluginInit
     def self.extended base
       puts "PluginInit extended by #{base}"
       puts base.class_eval("class << self; self; end").ancestors.inspect
-      #puts base.new.instance_eval("class << self; self; end").ancestors.inspect
     end
 
+    # Redefine initialize/new so as to call extend on new instances.
     def new *args, &block
       puts "--------- Initializing through PluginInit for #{self}"
       o = super
@@ -152,73 +235,40 @@ module Base
   module Plugin
     module Backward
       def self.unpack
+        # ne sert à rien dans cet exemple, mais pourrait être utile
+        # dans certains cas (send :include, ClassMethods par exemple)
         puts "Unpacking #{self}"
         puts
       end
 
+      # TODO
+      # peut-être imposer la convention que les classes de Base redéfinies ici
+      # sont à placer dans un module du même nom (Base, donc) ?
+      # Auquel cas, modifier en conséquence le hooking automatique dans Pluginable#activate etc.
+      # module Base
       module Speaker
-        #def self.included(receiver)
-          #puts "including #{self.inspect} into #{receiver}"
-          #receiver.send :include, InstanceMethods
-          #unless receiver.respond_to?(:old_say)
-            #puts "aliasing"
-            #receiver.instance_eval do
-              #alias :old_say :say
-            #end
-          #end
-        #end
-
-        #module InstanceMethods
-          def say(what)
-            super what.reverse
-          end
-        #end
+        def say(what)
+          super what.reverse
+        end
       end
+      #end
     end
   end
 end
 
 module Base
-  
+
   VERSION = 0.1
 
   class Speaker
     def initialize
-      puts instance_eval("class << self; self; end").ancestors.inspect
-      puts
-      #send :extend, Base::Plugins::Backward::SpeakerRedef
-
-      # - quand un plugin est chargé, Base::Plugin.activate se charge de :
-      #   - relever l'ensemble des modules définis par le plugin, avec un helper du genre :
-      #     Foo.constants.map { |s| Foo.const_get s }.grep(Class) # à affiner bien sûr
-      #   - étant donnée la liste des classes qui sont déclarées comme étant modifiables par les plugins :
-      #     - si le plugin tente de modifier une classe de Base qui n'est pas dans la liste autorisée, erreur et ne pas charger le plugin
-      #     - pour chacune des classes autorisée et modifiée par le plugin (module défini), ajouter une entrée dans plugins_hook
-      # pour le reste, pareil :
-      # - les classes modifiables extend un module qui redéfinit new, de façon à :
-      #   - appeler self.extend [les plugins associés à self] (self étant une instance d'une classe modifiable, dans ce contexte)
-      #   - appeler super
-      # - le extend est réalisé pour chaque plugin présent dans plugins_hooks[NomDeLaClasseDeLInstance]
-      #
-      # À terme, ce qui serait cool, c'est d'en faire un gem, assez souple. Par défaut, toutes les classes et modules, récursivement,
-      # d'un module ou d'une classe mère seraient modifiables (clés dans plugin_hooks), mais on pourrait faire un truc du genre :
-      # Pluginable.config do
-      #   only [Base::Server, Base::Config, Base::Speaker::Public]
-      #   # ou bien :
-      #   except [...]
-      #   # toute autre conf. utile
-      # end
-      #
-      # On aurait alors un DSL similaire à mon cas oneshot :
-      # - activate "plugin" pour activer un plugin et le relier à toutes les classes concernées
-      # - shutdown "plugin" ?
-      # et la même architecture des fichiers/dossiers.
-      puts instance_eval("class << self; self; end").ancestors.inspect
+      puts "A new speaker has been requested."
+      puts "ancestors: " + instance_eval("class << self; self; end").ancestors.inspect
       puts
     end
 
-    def say(what)
-      p "say: #{what}"
+    def say something
+      p "say: #{something}"
     end
   end
 
@@ -229,8 +279,6 @@ module Base
 
   class Server
     Base.extend Pluginable
-    #puts Base.class_eval("class << self; self; end").ancestors.inspect
-    #puts Base::Server.class_eval("class << self; self; end").ancestors.inspect
 
     s = Base::Speaker.new
     s.say 'hello world'
@@ -246,17 +294,23 @@ module Base
     s2 = Base::Speaker.new
     puts "-@-@-@-@-@-@-@-"
     s2.say 'hello world'
+
+    #TODO
+    #Base.shutdown :backward
+    # ie. la méthode shutdown pour virer le plugin (mais ça voudrait dire unextend
+    # et ça je sais pas si c'est possible ! Auquel cas il faudrait gruiker, genre
+    # undef les méthodes du module plugin, puis possiblité de les redef… Peut-être
+    # utiliser la technique des BlankObject avec hide et reveal ?)
+    #
+    # et la possiblitié de passer soit une chaîne, soit un symbole comme argument
+    # de activate et shutdown
+    # plus insensibilité à la casse, peut-être
   end
 
 end
 
-#puts Base.fetch_nested(recursive: true)
-
-#puts "---"
-
-#Base.fetch_nested(recursive: true) { |e| puts "#{e} (#{e.class})"; e.new if e.is_a? Class }
-
-#Base::Server.new
-
 Base::Server.new
+
+# TODO
+# specs!
 
